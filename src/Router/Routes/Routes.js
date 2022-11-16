80df1848c6763c3520df91e3ad6c104841c620f0
Salahuddin Ahmed <salahuddinahmed8544@gmail.com>
2022-11-15 18:32:38 -0800

    added private route

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../../Blog/Blog";
 import Main from "../../Layout/Main";
+import AllReview from "../../pages/AllReview/AllReview";
+import AllServices from "../../pages/AllServices/AllServices";
+import About from "../../pages/Home/About/About";
+import Details from "../../pages/Home/Home/Details/Details";
 import Home from "../../pages/Home/Home/Home";
 import Services from "../../pages/Home/Services/Services";
 import Login from "../../pages/Loging/Login";
+import ReviewForm from "../../pages/ReviewForm/ReviewForm";
 import SingUp from "../../pages/SignUp/SingUp";
 
 const router = createBrowserRouter([
@@ -30,6 +35,28 @@ const router = createBrowserRouter([
             {
                 path:'/services',
                 element:<Services></Services>
+            },
+            {
+                path:'/services/:id',
+                element:<Details></Details>,
+                loader:({params})=> fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path:'/allServices',
+                element:<AllServices></AllServices>
+            },
+            {
+                path:'/review/:id',
+                element:<ReviewForm></ReviewForm>,
+                loader:({params})=> fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path:'/allreview',
+                element:<AllReview></AllReview>,
+            },
+            {
+                path:'/about',
+                element:<About></About>
             }
         ]
     }
