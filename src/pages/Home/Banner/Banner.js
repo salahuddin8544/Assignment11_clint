d7e1d66ccd4f5abe3d2f43dc6ceff33125dd725b
Salahuddin Ahmed <salahuddinahmed8544@gmail.com>
2022-11-16 08:15:23 -0800

    complete silde

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -33,10 +33,10 @@ const Banner = () => {
     ]
     return (
         <div>
-           <div className='text-center text-2xl'>
+           <div className='text-center text-2xl '>
            <h3 className='pb-2 text-6xl'>Welcome to <br /> <br /> <span><span className='text-orange-500'>SU</span> Photography</span></h3>
            </div>
-            <div className="carousel w-full py-10">
+            <div className="carousel w-3/4 py-2 mx-auto">
         {
             bannerData.map(slide => <BannerItem
             key={slide.id} slide ={slide}></BannerItem>)
--- src/pages/Home/Banner/BannerItem.js
+++ src/pages/Home/Banner/BannerItem.js
@@ -3,9 +3,9 @@ import './BannerItem.css'
 const BannerItem = ({slide}) => {
     const {image,id,prev,next} = slide;
     return (
-        <div id={`slide${id}`} className="carousel-item relative w-full h-auto">
-        <div className='carousel-img'>
-        <img  src={image} className="w-full rounded-xl" />
+        <div id={`slide${id}`} className="carousel-item relative w-full h-96">
+        <div className='carousel-img w-full'>
+        <img  src={image} className="w-full rounded-xl  h-96"  />
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href={`#slide${prev}`} className="btn btn-circle me-5">❮</a> 
